25dce8aa0d63aee7f559b36dcaf99f54f712f45a
Nahid Hasan Utsob <mdnahidhassan555@gmail.com>
2025-09-29 15:40:27 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+      colors: {
+  primary: "#FF921B80",
+  secondary: "#F66844",
+}
+
+      },
+      backgroundImage: {
+        "gradient-main": "var(--gradient-main)",
+      },
+    },
+  },
+  plugins: [],
+};
